88940e438e6ead5c3c1a8b29d5a4bbbbd8fe3a11
SYF <32608999+SnekTech@users.noreply.github.com>
2025-05-24 16:55:05 +0800

    Initial commit

---
+++ src/Game/Scripts/Main.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace GameTemplate;
+
+public partial class Main: Node2D
+{
+    public override void _Ready()
+    {
+        GD.Print("Hello from pixel art template!");
+    }
+}
