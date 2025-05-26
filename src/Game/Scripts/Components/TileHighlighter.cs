545a1e458eff6e8e011179b69f33dc85d469161d
syf <2654525303@qq.com>
2025-05-26 22:20:50 +0800

    create the tile highlighter

---
+++ src/Game/Scripts/Components/TileHighlighter.cs
@@ -0,0 +1,56 @@
+﻿using Godot;
+
+namespace LabAutobattler.Components;
+
+public partial class TileHighlighter : Node
+{
+    [Export]
+    private bool enabled = true;
+    [Export]
+    private PlayArea playArea = null!;
+    [Export]
+    private TileMapLayer highlightLayer = null!;
+    [Export]
+    private Vector2I tile;
+
+    private int _sourceId;
+
+    public bool Enabled
+    {
+        get => enabled;
+        set
+        {
+            enabled = value;
+            if (enabled == false)
+            {
+                highlightLayer.Clear();
+            }
+        }
+    }
+
+    public override void _Ready()
+    {
+        _sourceId = playArea.TileSet.GetSourceId(0);
+    }
+
+    public override void _Process(double delta)
+    {
+        if (enabled == false)
+            return;
+
+        var selectedTile = playArea.GetHoveredTile();
+        if (playArea.IsTileInBounds(selectedTile) == false)
+        {
+            highlightLayer.Clear();
+            return;
+        }
+        
+        UpdateTile(selectedTile);
+    }
+
+    private void UpdateTile(Vector2I selectedTile)
+    {
+        highlightLayer.Clear();
+        highlightLayer.SetCell(selectedTile, _sourceId, tile);
+    }
+}
