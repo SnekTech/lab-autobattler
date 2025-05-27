52906ff03041f2e9da814b909bd6dec742b35007
syf <2654525303@qq.com>
2025-05-27 22:07:30 +0800

    create PlayerStats resource

---
+++ src/Game/Scripts/Data/Player/PlayerStats.cs
@@ -0,0 +1,44 @@
+﻿using Godot;
+
+namespace LabAutobattler.Data.Player;
+
+[GlobalClass]
+public partial class PlayerStats : Resource
+{
+    [Export(PropertyHint.Range, "0,99")]
+    public int Gold
+    {
+        get => _gold;
+        set
+        {
+            _gold = value;
+            EmitChanged();
+        }
+    }
+
+    [Export(PropertyHint.Range, "0,99")]
+    public int Xp
+    {
+        get => _xp;
+        set
+        {
+            _xp = value;
+            EmitChanged();
+        }
+    }
+
+    [Export(PropertyHint.Range, "1,10")]
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            _level = value;
+            EmitChanged();
+        }
+    }
+
+    private int _gold;
+    private int _xp;
+    private int _level;
+}
