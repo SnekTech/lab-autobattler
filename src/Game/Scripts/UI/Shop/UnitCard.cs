c2a631f04a68560c835aa92640db9cdee8374964
syf <2654525303@qq.com>
2025-05-29 10:41:19 +0800

    create the unit card script

---
+++ src/Game/Scripts/UI/Shop/UnitCard.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Godot;
+using GodotUtilities;
+using LabAutobattler.Data.Player;
+using LabAutobattler.Data.Units;
+
+namespace LabAutobattler.UI.Shop;
+
+[Scene]
+public partial class UnitCard : Button
+{
+    public event Action<UnitStats>? UnitBought;
+
+    [Export]
+    public PlayerStats PlayerStats { get; private set; } = null!;
+
+    [Export]
+    private UnitStats defaultUnitStats = null!;
+
+    [Node]
+    private Label Traits { get; set; } = null!;
+
+    [Node]
+    private Panel Bottom { get; set; } = null!;
+
+    [Node]
+    private Label UnitName { get; set; } = null!;
+
+    [Node]
+    private Label GoldCost { get; set; } = null!;
+
+    [Node]
+    private Panel Border { get; set; } = null!;
+
+    [Node]
+    private TextureRect UnitIcon { get; set; } = null!;
+
+    [Node]
+    private Panel EmptyPlaceholder { get; set; } = null!;
+
+    public UnitStats UnitStats
+    {
+        get => _unitStats ?? defaultUnitStats;
+        set
+        {
+            _unitStats = value;
+            UpdateUnit(_unitStats);
+        }
+    }
+
+    private StyleBoxFlat BorderStyle => (StyleBoxFlat)Border.GetThemeStylebox("panel");
+    private StyleBoxFlat BottomStyle => (StyleBoxFlat)Bottom.GetThemeStylebox("panel");
+
+    private static readonly Color HoverBorderColor = new("fafa82");
+
+    private UnitStats? _unitStats;
+    private Color _borderColor;
+    private bool _bought;
+
+    public override void _Ready()
+    {
+        UnitStats = defaultUnitStats;
+        UpdateWithPlayerStats();
+
+        UnitBought += unitStats =>
+        {
+            GD.Print($"bought unit: {unitStats}");
+            GD.Print($"gold: {PlayerStats.Gold}");
+        };
+    }
+
+    public override void _EnterTree()
+    {
+        PlayerStats.Changed += OnPlayerStatsChanged;
+        Pressed += OnPressed;
+        MouseEntered += OnMouseEntered;
+        MouseExited += OnMouseExited;
+    }
+
+    public override void _ExitTree()
+    {
+        PlayerStats.Changed -= OnPlayerStatsChanged;
+        Pressed -= OnPressed;
+        MouseEntered -= OnMouseEntered;
+        MouseExited -= OnMouseExited;
+    }
+
+    private void UpdateUnit(UnitStats unitStats)
+    {
+        _borderColor = UnitStats.RarityColors[unitStats.Rarity];
+        BorderStyle.BorderColor = _borderColor;
+        BottomStyle.BgColor = _borderColor;
+        UnitName.Text = unitStats.Name;
+        GoldCost.Text = unitStats.GoldCost.ToString();
+        var iconTexture = (AtlasTexture)UnitIcon.Texture;
+        iconTexture.Region = iconTexture.Region with { Position = unitStats.SkinCoordinates * Constants.CellSize };
+    }
+
+    private void UpdateWithPlayerStats()
+    {
+        var hasEnoughGold = PlayerStats.Gold >= UnitStats.GoldCost;
+        Disabled = hasEnoughGold == false;
+
+        if (hasEnoughGold || _bought)
+        {
+            this.SetModulateAlpha(1);
+        }
+        else
+        {
+            this.SetModulateAlpha(0.5f);
+        }
+    }
+
+    private void OnPlayerStatsChanged() => UpdateWithPlayerStats();
+
+    private void OnPressed()
+    {
+        if (_bought)
+            return;
+
+        PlayerStats.Gold -= UnitStats.GoldCost;
+        _bought = true;
+        EmptyPlaceholder.Show();
+        UnitBought?.Invoke(UnitStats);
+    }
+
+    private void OnMouseEntered()
+    {
+        if (Disabled)
+            return;
+
+        BorderStyle.BorderColor = HoverBorderColor;
+    }
+
+    private void OnMouseExited()
+    {
+        BorderStyle.BorderColor = _borderColor;
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+}
