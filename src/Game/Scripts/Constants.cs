4598d020194435c41f963e08a63df7a177dff563
syf <2654525303@qq.com>
2025-05-29 10:59:26 +0800

    create the reroll button

--- src/Game/Scripts/Constants.cs
+++ src/Game/Scripts/Constants.cs
@@ -7,6 +7,8 @@ public static class Constants
     public static readonly Vector2I CellSize = new(32, 32);
     public static readonly Vector2I HalfCellSize = new(16, 16);
     public static readonly Vector2I QuarterCellSize = new(8, 8);
+
+    public const int RerollCost = 2;
 }
 
 public static class GroupNames
---
+++ src/Game/Scripts/UI/Shop/Reroll.cs
@@ -0,0 +1,62 @@
+﻿using Godot;
+using GodotUtilities;
+using LabAutobattler.Data.Player;
+
+namespace LabAutobattler.UI.Shop;
+
+[Scene]
+public partial class Reroll : Button
+{
+    [Export]
+    private PlayerStats PlayerStats { get; set; } = null!;
+
+    [Node]
+    private HBoxContainer hBoxContainer = null!;
+
+    public override void _Ready()
+    {
+        UpdateWithPlayerStats(PlayerStats);
+    }
+
+    public override void _EnterTree()
+    {
+        Pressed += OnPressed;
+        PlayerStats.Changed += OnPlayerStatsChanged;
+    }
+
+    public override void _ExitTree()
+    {
+        Pressed -= OnPressed;
+        PlayerStats.Changed -= OnPlayerStatsChanged;
+    }
+
+    private void UpdateWithPlayerStats(PlayerStats playerStats)
+    {
+        var hasEnoughGold = playerStats.Gold >= Constants.RerollCost;
+        Disabled = hasEnoughGold == false;
+
+        if (hasEnoughGold)
+        {
+            hBoxContainer.SetModulateAlpha(1);
+        }
+        else
+        {
+            hBoxContainer.SetModulateAlpha(0.5f);
+        }
+    }
+
+    private void OnPressed()
+    {
+        PlayerStats.Gold -= 2;
+    }
+
+    private void OnPlayerStatsChanged() => UpdateWithPlayerStats(PlayerStats);
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+}
