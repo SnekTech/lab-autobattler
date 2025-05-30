e91d9b419333d3b98110f02ee38282b00c0981c3
syf <2654525303@qq.com>
2025-05-30 22:50:50 +0800

    connect the shop to the unit spawner

--- src/Game/Scripts/Arena.cs
+++ src/Game/Scripts/Arena.cs
@@ -1,6 +1,8 @@
 ﻿using Godot;
 using GodotUtilities;
 using LabAutobattler.Components;
+using LabAutobattler.Data.Units;
+using LabAutobattler.UI.Shop;
 using LabAutobattler.UnitManage;
 
 namespace LabAutobattler;
@@ -14,16 +16,22 @@ public partial class Arena : Node2D
     private UnitSpawner unitSpawner = null!;
     [Node]
     private SellPortal sellPortal = null!;
+    [Node]
+    private Shop shop = null!;
 
     public override void _EnterTree()
     {
         unitSpawner.UnitSpawned += OnUnitSpawned;
+        shop.UnitBought += OnUnitBought;
     }
 
     public override void _ExitTree()
     {
         unitSpawner.UnitSpawned -= OnUnitSpawned;
+        shop.UnitBought -= OnUnitBought;
     }
+
+    private void OnUnitBought(UnitStats unitStats) => unitSpawner.SpawnUnit(unitStats);
 
     private void OnUnitSpawned(Unit unit)
     {
